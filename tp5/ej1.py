b7c307203ff907225c1dd354cc0d2476c7439276
TomiHuspenina <thuspenina@gmail.com>
2023-10-05 23:36:23 -0300

    avanzado

--- tp5/ej1.py
+++ tp5/ej1.py
@@ -9,6 +9,7 @@ funcion=[]
 fDerivada=[]
 derivada=[]
 dif=[]
+newDerivada=[]
 h=0.1
 i=0
 c=0
@@ -18,6 +19,7 @@ while c <= 0.7:
     exponencial = math.exp(x[i])
     funcion.append(exponencial * math.cos(x[i]))
     derivada.append(exponencial*(math.cos(x[i])-math.sin(x[i])))
+    newDerivada.append(exponencial*(math.cos(x[i])-math.sin(x[i])))
 
     print(f"funcion {i}: {funcion[i]}")
     print(f"X: {x[i]}")
@@ -71,3 +73,50 @@ while c <= 0.7:
     i += 1
 
 #forma de los 5 puntos
+
+print(" ")
+print("Forma de los 5 puntos")
+i = 0
+c = 0
+newDif=[]
+print(" ")
+while c <= 0.7:
+
+    if c==0 or c==h:
+
+        newFuncionProg = ((-25 * funcion[i]) + (48 * funcion[i + 1]) - (36*funcion[i + 2]) + (16*funcion[i+3])-(3*funcion[i+4])) / (12 * h)
+        print(f"Derivada P {i}:  {newFuncionProg}")
+        newDif.append(abs(newDerivada[i]-newFuncionProg))
+
+    elif c == 0.7 or c == 0.7-h:
+
+        newFuncionRegre = ((3*funcion[i - 4]) - (16 * funcion[i - 3]) + (36 * funcion[i-2]) - (48*funcion[i-1]) + (25*funcion[i])) / (12 * h)
+        print(f"Derivada R {i}:  {newFuncionRegre} ")
+        newDif.append(abs(newDerivada[i]-newFuncionRegre))
+
+    else:
+
+        if c - h >= h and c + 2*h < len(funcion):
+           
+           newFuncionCentrada = ((funcion[i - 2]) - (8*funcion[i - 1]) + (8*funcion[i+1]) - (funcion[i+2])) / (12 * h)
+           print(f"Derivada C {i}:  {newFuncionCentrada} ")
+           newDif.append(abs(newDerivada[i]-newFuncionCentrada))
+
+    c += h
+    i += 1
+
+i = 0
+c = 0
+print(" ")
+while c <= 0.7:
+    print(f"derivada calculada {i}: {newDerivada[i]}")
+    c += h
+    i += 1
+
+i = 0
+c = 0
+print(" ")
+while c <= 0.7:
+    print(f"diferencia {i}: {newDif[i]}")
+    c += h
+    i += 1
--- tp5/ej2.py
+++ tp5/ej2.py
@@ -4,6 +4,8 @@ print("ejercicio 1")
 x=[]
 funcion=[]
 fDerivada=[]
+derivada=[]
+dif=[]
 h = 1  # Usar enteros para h
 i = 0
 c = 10  # Usar enteros para c
@@ -12,6 +14,8 @@ while c <= 20:
     x.append(c / 10.0)  # Convertir c a punto flotante para x
     exponencial = math.exp(3 * x[i])
     funcion.append(5 * exponencial * math.sin(2 * x[i]))
+    derivada.append(5*(exponencial*3*math.sin(2*x[i])+math.cos(2*x[i])*2*exponencial))
+
     print(f"funcion {i+1}: {funcion[i]}")
     print(f"X: {x[i]}")
     c += h
@@ -23,12 +27,31 @@ while c <= 20:
     if c == 10:
         funcionProg = ((-3 * funcion[i]) + (4 * funcion[i + 1]) - (funcion[i + 2])) / (2 * h / 10.0)  # Convertir h a punto flotante para la derivada
         print(f"Derivada P:  {funcionProg}")
+        dif.append(abs(derivada[i]-funcionProg))
     elif c == 20:
         funcionRegre = ((funcion[i - 2]) - (4 * funcion[i - 1]) + (3 * funcion[i])) / (2 * h / 10.0)  # Convertir h a punto flotante para la derivada
         print(f"Derivada R:  {funcionRegre} ")
+        dif.append(abs(derivada[i]-funcionRegre))
     else:
         if c - h >= 10 and c + h <= 20:  # Asegurarse de que los valores estén dentro del rango adecuado
             funcionCentrada = ((funcion[i + 1]) - (funcion[i - 1])) / (2 * h / 10.0)  # Convertir h a punto flotante para la derivada
             print(f"Derivada C:  {funcionCentrada} ")
+            dif.append(abs(derivada[i]-funcionCentrada))
+    c += h
+    i += 1
+
+i = 0
+c = 10
+print(" ")
+while c <= 20:
+    print(f"derivada calculada {i}: {derivada[i]}")
+    c += h
+    i += 1
+
+i = 0
+c = 10
+print(" ")
+while c <= 20:
+    print(f"diferencia {i}: {dif[i]}")
     c += h
     i += 1
